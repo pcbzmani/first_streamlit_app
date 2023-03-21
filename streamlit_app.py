026ad6bcc240d15b248c189209266709cf3f7bfa
pcbzmani <pcbzmani@gmail.com>
2023-03-21 07:13:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ except URLError as e:
 
 streamlit.header("The fruit load list contains:")
 def get_fruit_load_list():
-  with my_cnx.curosr() as my_cur:
+  with my_cnx.cursor() as my_cur:
     my_cur.execute("SELECT * from pc_rivery_db.public.fruit_load_list")
     return my_cur.fetchall()
 
